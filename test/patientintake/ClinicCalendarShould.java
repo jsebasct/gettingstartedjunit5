2b942309660fb1d212f840bb39a86e1799857ff3
scruz <jsebas.ct@gmail.com>
2022-12-31 16:39:07 -0400

    module1 small changes

--- test/patientintake/ClinicCalendarShould.java
+++ test/patientintake/ClinicCalendarShould.java
@@ -16,14 +16,17 @@ class ClinicCalendarShould {
       calendar.addAppointment("Jim", "Weaver", "avery",
          "09/01/2018 2:00 pm");
       List<PatientAppointment> appointments = calendar.getAppointments();
+
       assertNotNull(appointments);
       assertEquals(1, appointments.size());
+
       PatientAppointment enteredAppt = appointments.get(0);
       assertEquals("Jim", enteredAppt.getPatientFirstName());
       assertEquals("Weaver", enteredAppt.getPatientLastName());
-      assertEquals(Doctor.avery, enteredAppt.getDoctor());
+      assertSame(Doctor.avery, enteredAppt.getDoctor());
       assertEquals("9/1/2018 02:00 PM",
-         enteredAppt.getAppointmentDateTime().format(DateTimeFormatter.ofPattern("M/d/yyyy hh:mm a", Locale.US)));
+         enteredAppt.getAppointmentDateTime()
+                 .format(DateTimeFormatter.ofPattern("M/d/yyyy hh:mm a", Locale.US)));
    }
 
 }
